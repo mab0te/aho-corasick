74102b5630dbf635e58200d400fa68192b86a7fc
T3asp0on <adrien.smondack@gmail.com>
2013-12-16 10:58:23 +0100

    Bilbliothèque des alphabets
    2, 4, 20 et 70 caractères

---
+++ alpha.h
@@ -0,0 +1,7 @@
+#ifndef _AC_ALPHA
+#define _AC_ALPHA
+
+#define TWO_CHAR_ALPHA "ab"
+#define FOUR_CHAR_ALPHA "abcd"
+#define TWENTY_CHAR_ALPHA "abcdefghij0123456789"
+#define SEVENTY_CHAR_ALPHA "abcdefghijklmnopqrstuvwxyz0123456789&~#'{([-|`_^@)]=}/*-+,?;.:!§*µ$£<>"
